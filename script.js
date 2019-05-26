232e794f445e8b53dcc2de134bf6644fa7f211b6
k-ulanov <ulanovconstantine@gmail.com>
2019-05-26 18:49:03 +0300

    Timing 50

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function move_interval(obj,speed,pos,time) {
 
 function position_trottling() {
 	clearTimeout(timer);
-	timer = setTimeout(position, 20);
+	timer = setTimeout(position, 50);
 
 
 	place_frame(info_slider);
